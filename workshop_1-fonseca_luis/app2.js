fbdbdfe26585765ad7ea88bbb6a53fb7850280dc
exeluis_laptop <exe.niklas@googlemail.com>
2016-04-09 14:58:56 +0200

    Aufgabe 1 mit asynchronen Funktionen gelöst

---
+++ workshop_1-fonseca_luis/app2.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+function printTower(data){
+	for(var i = 0; i < data.wolkenkratzer.length ;i++){
+		console.log('Name: ' + data.wolkenkratzer[i].name);
+		console.log('Stadt: ' + data.wolkenkratzer[i].stadt);
+		console.log('Höhe: ' + data.wolkenkratzer[i].hoehe);
+		console.log('--------------------------');
+	};
+};
+var data = fs.readFile('./wolkenkratzer.json', function(err, data) {
+	if(err) throw err;
+	data = JSON.parse(data);
+	data.wolkenkratzer.sort(function(a, b){
+		return b.hoehe - a.hoehe;
+	});
+	printTower(data);
+});
