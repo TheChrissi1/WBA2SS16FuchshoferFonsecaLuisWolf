2fe0819b27043bed63f6a7405755427c8adda561
Basti <bastian.f.1993@web.de>
2016-04-17 17:33:44 +0200

    Eigener Ordner ohne Fremdinhalte

---
+++ workshop_fuchshofer/aufgabe1.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+
+
+
+function towerAusgeben(data) {
+    for (var i=0; i< data.wolkenkratzer.length; i++) {
+        console.log("Name: " + data.wolkenkratzer[i].name);
+        console.log("Stadt: " + data.wolkenkratzer[i].stadt);
+        console.log("Hoehe: " + data.wolkenkratzer[i].hoehe);
+        console.log("___________________________________________");
+        };
+    };
+
+
+
+
+
+
+var data = fs.readFile("./wolkenkratzer.json", function(err, data) {
+    if (err) throw err;
+    data = JSON.parse(data);
+    towerAusgeben(data);
+});
+    
