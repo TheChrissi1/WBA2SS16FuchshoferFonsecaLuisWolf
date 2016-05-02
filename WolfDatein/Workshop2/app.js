98de5e5b7805e2dd14bebc28ccbfe34250cfac4b
Christian Wolf <Christian.Wolf@papss.de>
2016-05-02 10:42:05 +0200

    Workshop Aufgabe 2 - Bearbeitung angefangen

---
+++ WolfDatein/Workshop2/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+var serverPort = 1337;
+
+var users = [
+    {name: "Hans", age: 21},
+    {name: "Peter", age: 26}
+];
+
+app.get('/users', function(req, res){
+    
+    if (req.query.age !== undefined) {
+        
+        res.json(users.fliter(function(e, i, arr) {
+            return e.age == req.query.age
+        }));
+    } else {
+        res.json(users);
+    }
+});
+
+app.listen(serverPort, function(){
+    console.log('App listens on Port ' + serverPort);
+});
