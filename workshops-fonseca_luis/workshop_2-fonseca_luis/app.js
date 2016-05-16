49e2a568cab4b52a5d25be16850bdb17eac8495f
exeluis_laptop <exe.niklas@googlemail.com>
2016-05-16 17:22:49 +0200

    workshop2

--- workshops-fonseca_luis/workshop_2-fonseca_luis/app.js
+++ workshops-fonseca_luis/workshop_2-fonseca_luis/app.js
@@ -1,11 +1,23 @@
-var express = require('express');
+var express = require( 'express' );
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require( 'body-parser' );
 var jsonParser = bodyParser.json();
+var fs = require( 'fs' );
 
 
 var serverPort = 1337;
 
-var anime = require('./profil.json');
-var anime = anime.anime;
-console.log(anime[0].name);
+
+
+
+app.get('/anime/shingeki-no-kyojin', jsonParser, function(req, res){
+	var anime = require('./profil.json');
+	anime = anime.anime;
+	res.send(anime);
+});
+
+
+
+app.listen(serverPort, function(){
+	console.log('Server running on Port: ' + serverPort);
+});
