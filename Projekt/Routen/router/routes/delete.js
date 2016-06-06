9d2bbb6162170f902c44d67f35498b3fc56f759a
LittleGundam <bastian.f.1993@web.de>
2016-06-06 10:13:35 +0200

    fixed DELETE routes

--- Projekt/Routen/router/routes/delete.js
+++ Projekt/Routen/router/routes/delete.js
@@ -32,22 +32,17 @@ function loadStats() {
 //Gibt einen Benutzer anhand seiner ID (querry parameter) zurück.
 function returnUser (string) {
     
-//    var user = fs.readFileSync(__parentdirectory+"/daten/user.json")
-//    user = JSON.parse(user);
     var data = loadUser();
     
-    var erg = 0;
-    for (i=0; i<data.user.length; i++) {
+    var return_value = -1;
+    for (var i=0; i<data.user.length; i++) {
 
-        if (data.user[i].id == string) {
-            var erg = data.user[i];
-            return erg;
+        if (data.user[i].uID == string) {
+            return_value = i;
+            return return_value;
         }
     }
-    if (erg==0) {
-        erg = 'user not found!';
-        return erg;
-    }
+    return return_value;
 
 }
 //Gibt die Statistik eines Benutzers anhand seiner ID (querry parameter) zurück.
@@ -112,18 +107,18 @@ function anime_sort(string){
 }
 
 
-//[NOT OK]
+//[OK]
 router.delete( '/anime/:anime_name', jsonParser, function(req, res){
 	var data = loadProfile();
-	data = data.anime;
+	//data = data.anime;
 	var querry = req.params.anime_name;
 	var search = returnAnime(querry);
 	if(search != -1){
-		data.splice(search, 1);
-        /*
+		data.anime.splice(search, 1);
+        
         data = JSON.stringify( data, null, 4 );
         fs.writeFileSync(__parentdirectory+"/daten/profil.json", data);
-        */
+        
         
 		res.send("Anime successfully deleted: " + querry);
         console.log("DELETE anime: " + querry);
@@ -131,31 +126,30 @@ router.delete( '/anime/:anime_name', jsonParser, function(req, res){
 		res.send("Anime was not in list: " + querry);
         console.log("DELETE anime FAILED: " + querry);
 	}
-    console.log("ldasd");
     anime_sort(data);
 	
 });
 
 //[NOT OK]
-router.delete( '/user/:id', jsonParser, function(req, res){
+router.delete( '/user/:uID', jsonParser, function(req, res){
     
-    var querry = req.params.id;
+    var querry = req.params.uID;
     var data = loadUser();
-    var erg = 0;
-    var i;
-    for (i = 0; i<data.length; i++) {
-        if (data[i].id == querry) {
-            erg = i;
-        }
-    }
-    data.splice(i, 1);
-    if (erg != 0) {
+    var erg = returnUser(querry);
+    
+    
+    if (erg != -1) {
+        data.user.splice(erg, 1);
+        data = JSON.stringify(data, null, 4);
+        fs.writeFileSync(__parentdirectory+"/daten/user.json", data);
+        
         res.send("User successfully deleted " + querry);
         console.log("DELETE user: " + querry);
     } else {
         res.send("User was not in list: " + querry);
         console.log("DELETE user FAILED: " + querry);
     }
+    user_sort(data);
    
 /*
 	
@@ -164,7 +158,7 @@ router.delete( '/user/:id', jsonParser, function(req, res){
 		}  else {
 			res.status(404).type('text').send('User not found!');
 		}
-        */
+*/
 	
 });
 
