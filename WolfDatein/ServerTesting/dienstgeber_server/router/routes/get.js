2b31049901f0cdc8d88497aebc9147d2a8954f5f
Christian Wolf <Christian.Wolf@papss.de>
2016-06-15 16:52:23 +0200

    Aktueller Server zum testen kopiert

---
+++ WolfDatein/ServerTesting/dienstgeber_server/router/routes/get.js
@@ -0,0 +1,142 @@
+var router = express.Router();
+
+//[OK]
+//Index
+router.get('/', function(req, res){
+	res.status(200).type('text').send('Projekt der TH Köln, Medieninformatik 4. Semester.');
+    console.log('Projekt der TH Köln, Medieninformatik 4. Semester.');
+});
+
+//[OK]
+//Gibt eine Liste aller Animes aus.
+router.get('/anime', jsonParser, function(req, res){
+
+    db.keys('anime:*',function(err,rep) {
+
+		var anime = [];
+
+		if (rep.length == 0) {
+			
+            console.log('keine animes vorhanden');
+            res.status(404).type('text').send('no Animes found');
+			
+		} else if (rep.length > 0) {
+            db.mget(rep, function(err,rep) {
+                rep.forEach(function(val){
+                    anime.push(JSON.parse(val));
+			    });
+
+                res.status(200).type('json').send(anime);
+                //res.set("Content-Type", 'application/json').status(200).json(anime).end();
+                
+            });
+        }
+	});
+ 
+});
+
+//[OK]
+//Gibt einen Anime anhand seines Namens (querry-parameter) zurück.
+router.get('/anime/:anime_name', jsonParser, function(req, res){
+    
+    db.get('anime:'+req.params.anime_name, function(err, rep) {
+
+		if (rep) {
+			res.status(200).type('json').send(rep);
+		} else {
+			res.status(404).type('text').send('Anime not found!');
+		}
+	});
+
+});
+
+//[OK] - kein json format?
+//Gibt eine Liste aller Benutzer aus.
+router.get('/user', jsonParser, function(req, res){
+
+    db.keys('user:*',function(err,rep) {
+
+		var users = [];
+
+		if (rep.length == 0) {
+            console.log('keine user vorhanden');
+            res.status(404).type('text').send('no user found');
+		
+        } else if (rep.length > 0) {
+            db.mget(rep, function(err,rep) {
+                rep.forEach(function(val){
+				    users.push(JSON.parse(val));
+			    });
+
+              res.status(200).type('json').send(users);  
+            });
+        }
+	});
+});
+
+//[OK]
+//Gibt einen Benutzer anhand seiner ID (querry-parameter) zurück.
+router.get('/user/:uID', jsonParser, function(req, res){
+
+    db.get('user:'+req.params.uID, function(err, rep) {
+
+		if (rep) {
+			res.status(200).type('json').send(rep);
+		} else {
+			res.status(404).type('text').send('User not found!');
+		}
+	});
+    
+});
+
+//[OK]
+//Gibt die Statistik eines Benutzers aus.
+router.get( '/user/:uID/stats', jsonParser, function(req, res){
+    
+  
+    db.get('stats:'+req.params.uID, function(err, rep) {
+
+		if (rep) {
+			res.status(200).type('json').send(rep);
+		} else {
+			res.status(404).type('text').send('Stats/User not found!');
+		}
+	});
+
+
+});
+
+//[NOT OK]
+//Gibt eine Liste der Genres aus.
+router.get('/genre', jsonParser, function(req, res) {
+    
+    
+});
+
+//[NOT OK]
+//Gibt eine Liste der Referenzen aus.
+router.get('/ref', jsonParser, function(req, res) {
+    
+    
+});
+
+//[NOT OK]
+//Gibt Informationen zu einer Referenz aus.
+router.get('/ref/:refID', jsonParser, function(req, res) {
+    
+    
+});
+
+//[NOT OK]
+//Gibt eine spezifizierung der Animetabelle aus.
+router.get('/anime/filter/:para', jsonParser, function(req, res) {
+    
+    //Anhand der querry parameter kann die profil.json nach bestimmten 
+    //kriterien wie genre, anzahl folgen etc. durchsucht werden.
+    // localhost.de/anime/filter/?genre=action&folgen=500
+    
+});
+
+
+console.log('loaded get.js');
+module.exports = router;
---
+++ WolfDatein/ServerTesting/dienstgeber_server/router/routes/put.js
@@ -0,0 +1,85 @@
+var router = express.Router();
+
+//[OK]
+//Trägt einen neuen Anime in die DB ein.
+router.put('/anime', jsonParser, function( req, res){
+    
+    var newAnime = req.body;
+    db.set('anime:'+newAnime.name, JSON.stringify(newAnime), function(err, rep) {
+        res.status(201).type('text').send('new anime: ' +newAnime.name);
+    });
+
+});
+
+//[OK]
+// Ändert die Daten eines Animes.
+router.put( '/anime/:anime_name', jsonParser, function(req, res){
+
+	db.exists('anime:'+req.params.anime_name, function(err, rep) {
+		if (rep == 1) {
+			var updatedAnime = req.body;
+		//	updatedAnime.name = req.params.anime_name;
+
+			db.set('anime:' + req.params.anime_name, JSON.stringify(updatedAnime), function(err, rep) {
+				res.status(200).type('json').send(updatedAnime);
+			});
+		} else {
+			res.status(404).type('text').send('Anime not exists!');
+        }
+		
+	});
+});
+
+//[OK]
+//Trägt einen neuen Benutzer in die DB ein.
+router.put('/user', jsonParser, function(req, res){
+
+    var newUser = req.body;
+    
+    //Inkrement wird nicht zurückgesetzt wenn bspw. alle User gelöscht werden!!!
+	db.incr('uID:user', function (err, rep) {
+
+		newUser.uID = rep;
+		db.set('user:'+newUser.uID, JSON.stringify(newUser), function(err, rep) {
+			res.status(201).type('text').send('new user: ' +newUser.uID);
+		});
+        var newStat = {"uID": newUser.uID,"gesFolgen": "","gesAnimes": ""};
+        db.set('stats:'+newUser.uID, JSON.stringify(newStat), function(err, rep) {
+            console.log('new statistic for user: ' +newUser.uID);
+        });
+	});
+});
+
+//[OK]
+//Ändert die Daten eines Benutzers.
+router.put( '/user/:uID', jsonParser, function(req, res){
+    
+    	db.exists('user:'+req.params.uID, function(err, rep) {
+		if (rep == 1) {
+			var updatedUser = req.body;
+			updatedUser.uID = req.params.uID;
+
+			db.set('user:' + req.params.uID, JSON.stringify(updatedUser), function(err, rep) {
+				res.status(200).type('json').send(updatedUser);
+			});
+		} else {
+			res.status(404).type('text').send('User already exists!');
+		}
+	});
+});
+
+//[NOT OK]
+//Ändert die Statistik eines Nutzers.
+router.put( '/user/:id/stats', jsonParser, function(req, res){
+    
+    
+
+});
+
+
+
+
+
+
+console.log('loaded put.js.')
+module.exports = router;
