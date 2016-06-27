b3dde3008199c3eb983397e0fdfd74828a54384a
LittleGundam <bastian.f.1993@web.de>
2016-06-27 21:04:22 +0200

    update anime filter und implementierung von Vorschlägen

--- workshops-fonseca_luis/workshop_4+/passport_tests/Server/dienstnutzer_server/router/routes/get.js
+++ workshops-fonseca_luis/workshop_4+/passport_tests/Server/dienstnutzer_server/router/routes/get.js
@@ -1,6 +1,10 @@
 var router = express.Router();
 var http = require('http');
 
+//Wird bei Server start erzeugt und mit der Zeit mit Vorschlägen gefüllt
+var suggestions = {
+    "data":[]
+}
 
 function isAuthenticated( req, res, next){
 	console.log(req.isAuthenticated);
@@ -10,6 +14,24 @@ function isAuthenticated( req, res, next){
 		router.get('/index');
 	}
 };
+
+//Funktion zum überprüfen ob ein Vorschlag bereits vorhanden ist
+function check (data, newData) {
+    var anime = data;
+    var value = 1;
+    for (var i = 0; i < anime.data.length; i++) {
+        //Ist der Anime-Vorschlag schon vorhanden?
+        if (anime.data.length == 0) {
+            value = 0;
+        }
+        if (anime.data[i].name == newData.name && anime.data.length != 0) {
+            value = 0;
+            break;
+        }
+    }
+    return value;
+}
+
 //[OK]
 //Index
 
@@ -58,10 +80,6 @@ router.get('/anime', function(req, res){
 //Gibt eine spezifizierung der Animetabelle aus.
 router.get('/anime/filter:para', jsonParser, function(req, res) {
 
-    //Anhand der querry parameter kann die profil.json nach bestimmten
-    //kriterien wie genre, anzahl folgen etc. durchsucht werden.
-    // localhost.de/anime/filter/?genre=action&folgen=500
-    // genre=name, episodes=zahl, name, release?date, dub, sub
     var options = {
         host: "localhost",
         port: 3000,
@@ -72,51 +90,226 @@ router.get('/anime/filter:para', jsonParser, function(req, res) {
         }
     }
 
+    var tmp = 0;
+    var countEpisodes = 0;
+    var countGenre = 0;
+    var countName = 0;
+    var countMax = 0;
+    var countMin = 0;
     var query = req.params.para;
     var queryL = query.length;
-    //// console.log(query);
-    query = query.substring(1, query.length);
-    //// console.log(query);
+
+    //Bestimmt die Anzahl vom substring "episodes"
+    var tmpEpisodes = query;
+    for (var t=0; t<tmpEpisodes.length; t++ ) {
+        var posEpi;
+        tmpEpisodes = tmpEpisodes.substr(posEpi+t,tmpEpisodes.length);
+        posEpi = tmpEpisodes.indexOf("episodes");
+        if (posEpi == -1) {
+            break;
+        } else {
+            countEpisodes++;
+        }
+    }
+
+    //Bestimmt die Anzahl vom substring "genre"
+    var tmpGenre = query;
+    for (var u=0; u<tmpGenre.length; u++ ) {
+        var posGen;
+        tmpGenre = tmpGenre.substr(posGen+u,tmpGenre.length);
+        posGen = tmpGenre.indexOf("genre");
+        if (posGen == -1) {
+            break;
+        } else {
+            countGenre++;
+        }
+    }
+    
+     //Bestimmt die Anzahl vom substring "name"
+    var tmpName = query;
+    for (var t=0; t<tmpName.length; t++ ) {
+        var posName;
+        tmpName = tmpName.substr(posName+t,tmpName.length);
+        posName = tmpName.indexOf("name");
+        if (posName == -1) {
+            break;
+        } else {
+            countName++;
+        }
+    }
+    
+    //Bestimmt die Anzahl vom substring "maxEpisodes"
+    var tmpMax = query;
+    for (var t=0; t<tmpMax.length; t++ ) {
+        var posMax;
+        tmpMax = tmpMax.substr(posMax+t,tmpMax.length);
+        posMax = tmpMax.indexOf("maxEpisodes");
+        if (posMax == -1) {
+            break;
+        } else {
+            countMax++;
+        }
+    }
+    
+    //Bestimmt die Anzahl vom substring "minEpisodes"
+    var tmpMin = query;
+    for (var t=0; t<tmpMin.length; t++ ) {
+        var posMin;
+        tmpMin = tmpMin.substr(posMin+t,tmpMin.length);
+        posMin = tmpMin.indexOf("minEpisodes");
+        if (posMin == -1) {
+            break;
+        } else {
+            countMin++;
+        }
+    }
+    
+    //console.log('cEpi: ' +countEpisodes);
+    //console.log('cGen: ' +countGenre);
+    //console.log('cName: ' +countName);
+    //console.log('cMax: ' +countMax);
+    //console.log('cMin: ' +countMin);
+    
+    //Entfernt das "?" an pos[0] aus dem string
+    query = query.substr(1,query.length);
+    
+    //Parst den querystring in ein Objekt
     var erg = queryString.parse(query);
+    
 
-
+    //Request auf den Dienstgeber
+    
+    //Da im "erg"-Objekt sowohl arrays als auch normale variablenzuweisungen vorkommen können, muss über die Anzahl
+    //der im querystring vorkommenden Parameter (countEpisodes, count...) auf den Wert der Parameter zugegriffen werden.
     var exReq = http.request(options, function(exRes){
 
         exRes.on("data", function(chunk){
-
+            
+            //Animeliste aus dem Dienstgeber
             var exAnimeList = JSON.parse(chunk);
+            //Neue Animeliste
             var animeList = [];
-
-            if (erg.episodes == null) {
-            } else {
-                for (var h=0; h<erg.episodes.length; h++) {
-
+            
+            // Überprüfen ob Filter "Episodes" mit mindestens einem Anime übereinstimmt.
+            if (countEpisodes == 1) {
                     for (var i=0; i<exAnimeList.length; i++) {
-
-                        if (exAnimeList[i].episodes == erg.episodes[h]) {
+                        if (exAnimeList[i].episodes == erg.episodes) {
                             animeList.push(exAnimeList[i]);
+                            
+//                            //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+//                            if (check(suggestions, {"name":exAnimeList[i].name}) == 1) {
+//                                suggestions.data.push({"name":exAnimeList[i].name});
+//                            }
+                        }
+                    }      
+            } else if( countEpisodes > 1) {
+                for (var j=0; j<=countEpisodes; j++) {
+                    for (var k=0; k<exAnimeList.length; k++) {
+                        if (exAnimeList[k].episodes == erg.episodes[j]) {
+                            animeList.push(exAnimeList[k]);
+                            
+//                            //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+//                            if (check(suggestions, {"name":exAnimeList[k].name}) == 1) {
+//                                suggestions.data.push({"name":exAnimeList[k].name});
+//                            }
+                        }
+                    }
+                }    
+            }
+            
+            //Überprüft ob Filter "maxEpisodes" vorhanden ist und filtert die Liste danach
+            if (countMax == 1) {
+                    for (var a=0; a<exAnimeList.length; a++) {
+                        if (exAnimeList[a].episodes <= erg.maxEpisodes) {
+                            animeList.push(exAnimeList[a]);
+                           
+//                            //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+//                            if (check(suggestions, {"name":exAnimeList[a].name}) == 1) {
+//                                suggestions.data.push({"name":exAnimeList[a].name});
+//                            }
+                        }
+                    }
+            }
+                
+            //Überprüft ob Filter "minEpisodes" vorhanden ist und filtert die Liste danach
+            if (countMin == 1) {
+                    for (var b=0; b<exAnimeList.length; b++) {
+                        if (exAnimeList[b].episodes >= erg.minEpisodes) {
+                            animeList.push(exAnimeList[b]);
+                            
+//                            //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+//                            if (check(suggestions, {"name":exAnimeList[b].name}) == 1) {
+//                                suggestions.data.push({"name":exAnimeList[b].name});
+//                            }
+                        }
+                    }
+            }
+           
+               
+            // Überprüfen ob Filter "Genre" mit mindestens einem Anime übereinstimmt.
+            if (countGenre == 1) {
+                    for (var m=0; m<exAnimeList.length; m++) {
+                        if (exAnimeList[m].genre.indexOf(erg.genre) > -1) {
+                            animeList.push(exAnimeList[m]);
+                            
+//                            //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+//                            if (check(suggestions, {"name":exAnimeList[m].name}) == 1) {
+//                                suggestions.data.push({"name":exAnimeList[m].name});
+//                            }
+                        }
+                    }
+                  
+            } else if (countGenre > 1) {
+                for (var n=0; n<=countGenre; n++) {
+                    for (var o=0; o<exAnimeList.length; o++) {
+                        if (exAnimeList[o].genre.indexOf(erg.genre[n]) > -1) {
+                            animeList.push(exAnimeList[o]);
+                            
+//                            //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+//                            if (check(suggestions, {"name":exAnimeList[o].name}) == 1) {
+//                                suggestions.data.push({"name":exAnimeList[o].name});
+//                            }   
                         }
                     }
                 }
             }
-
-            if (erg.genre == null) {
-            } else {
-                for (var h=0; h<erg.genre.length; h++) {
-
-                    for (var i=0; i<exAnimeList.length; i++) {
-
-                        if (exAnimeList[i].genre == erg.genre[h]) {
-                            animeList.push(exAnimeList[i]);
+            
+            // Überprüfen ob Filter "Name" mit mindestens einem Anime übereinstimmt.
+            if (countName == 1) {
+                    for (var m=0; m<exAnimeList.length; m++) {
+                        //Alle 3 möglichen Namen eines Animes werden verglichen
+                        if (exAnimeList[m].name.indexOf(erg.name) > -1 || exAnimeList[m].name_en.indexOf(erg.name) > -1 ||      exAnimeList[m].name_de.indexOf(erg.name) > -1){
+                            animeList.push(exAnimeList[m]);
+                            
+//                            //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+//                            if (check(suggestions, {"name":exAnimeList[m].name}) == 1) {
+//                                suggestions.data.push({"name":exAnimeList[m].name});
+//                            }
+                        }
+                    }
+                  
+            } else if (countName > 1) {         
+                for (var n=0; n<=countName; n++) {
+                    for (var o=0; o<exAnimeList.length; o++) {
+                        //Alle 3 möglichen Namen eines Animes werden verglichen
+                        if (exAnimeList[o].name.indexOf(erg.name[n]) > -1 || exAnimeList[o].name_en.indexOf(erg.name[n]) > -1 || exAnimeList[o].name_de.indexOf(erg.name[n]) > -1) {
+                            animeList.push(exAnimeList[o]);
+                            
+//                            //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+//                            if (check(suggestions, {"name":exAnimeList[o].name}) == 1) {
+//                                suggestions.data.push({"name":exAnimeList[o].name});
+//                            }    
                         }
                     }
                 }
             }
+            
             if (animeList == '') {
                 res.render('pages/noResult');
                 res.end;
-            } else {
-                res.render('pages/anime',{animeList:animeList});
+            } else {     
+                //Wirklich auf /anime rendern?? Eventuell neue .ejs mit /anime/filter machen??
+                res.render('pages/anime',{animeList:animeList,suggestions:suggestions});
                 res.end();
             }
         });
@@ -213,8 +406,14 @@ router.get('/anime/:anime_name', jsonParser, function(req, res){
 										tmp = index + 1;
 									}
 								}
+                
+                //Überprüft ob Vorschlag bereits existiert, sonst wird er eingetragen
+                                    if (check(suggestions, {"name":animeData.name}) == 1) {
+                                        suggestions.data.push({"name":animeData.name});
+                                    }
+                
 								refData = ref_ids;
-								res.render('pages/animeID',{animeData:animeData, refData:refData});
+								res.render('pages/animeID',{animeData:animeData, refData:refData,suggestions:suggestions});
 								res.end();
 								console.log(ref_ids);
 						});
