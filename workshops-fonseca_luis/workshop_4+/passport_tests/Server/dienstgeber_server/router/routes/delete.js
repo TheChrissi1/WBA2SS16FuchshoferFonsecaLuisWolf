e702986f3f194a098927c1d86e9588f9a2f2fa3b
LittleGundam <bastian.f.1993@web.de>
2016-06-28 15:32:30 +0200

    update DG delete

--- workshops-fonseca_luis/workshop_4+/passport_tests/Server/dienstgeber_server/router/routes/delete.js
+++ workshops-fonseca_luis/workshop_4+/passport_tests/Server/dienstgeber_server/router/routes/delete.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.delete( '/anime/:anime_name', jsonParser, function(req, res){
 
 	db.del('anime:'+req.params.anime_name, function(err, rep){
+        
 		if (rep == 1){
 			res.status(200).type('text').send('Anime successfully deleted');
 		}  else {
@@ -18,13 +19,21 @@ router.delete( '/anime/:anime_name', jsonParser, function(req, res){
 //--> Inkrement der uID wird nicht gelöscht!! Nach vollständigem löschen wird trotzdem ab letzter uID weitergezählt.
 router.delete( '/user/:user_id', jsonParser, function(req, res){
 
-    db.del('user:'+req.params.user_id, function(err, rep){
-		if (rep == 1){
-			res.status(200).type('text').send('User successfully deleted');
-		}  else {
-			res.status(404).type('text').send('User not found!');
-		}
-	});
+    db.get('user:'+req.params.user_id, function(err, rep) {
+        
+        if (rep) {
+            var user = JSON.parse(rep);
+            user.active = false;
+            db.set('user:' +req.params.user_id, JSON.stringify(user), function(err, rep) {
+                res.status(200).type('text').send('User deactivated!');
+            });
+            
+            //res.status(200).type('text').send('User deactivated!');
+        } else {
+            res.status(404).type('text').send('User not found!');
+        }
+    })
+
 
 });
 
